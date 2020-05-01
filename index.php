68626edf783cf13225a0307d9aff918f043dee93
Tom Woodward <twwoodward@me.com>
2020-05-01 14:37:57 -0400

    sends email, changed trigger for pw creation
    remember that _customfields are hidden from admin view or you'll think you are crazy

--- index.php
+++ index.php
@@ -23,19 +23,12 @@ function closed_query_vars( $qvars ) {
 add_filter( 'query_vars', 'closed_query_vars' );
 
 
-//add secret password
-add_action('transition_post_status', 'closer_add_meta_pw', 10);
-function closer_add_meta_pw(){
-  global $post;
-  $pw = wp_generate_password(22, true);//create pw
-  add_post_meta($post->ID, '_closer_pw', $pw, true );//assign to custom field but don't make any new ones or update if it exists
-}
+
 
 add_action('the_post', 'closer_add_tag');
 
 function closer_add_tag(){
   global $post;
-  var_dump(get_post_meta($post->ID, '_closer_pw', true));
   if ( get_query_var('closed',1) && get_query_var('pw',1) ) {
     $closed = get_query_var('closed',1);
     $url_pw = get_query_var('pw',1);
@@ -65,15 +58,23 @@ if ( ! function_exists('write_log')) {
 
   //print("<pre>".print_r($a,true)."</pre>");
 
+//add secret password
+function closer_add_meta_pw($post_id){
+  $pw = wp_generate_password(22, true);//create pw
+  add_post_meta($post_id, '_closer_pw', $pw, true );//assign to custom field but don't make any new ones or update if it exists
+}
+
 //SEND THE EMAIL
 add_action( 'gform_after_submission_1', 'closer_send_email', 10, 2 );
 function closer_send_email( $entry, $form ) {
- 
-    //getting post ID
-    $post_id = $entry['post_id'];
+    
+    $post_id = $entry['post_id'];//get created ID
+
+    closer_add_meta_pw($post_id);//add pw to custom field
+
     $email = rgar( $entry, '3' );
     $closer_pw = get_post_meta($post_id, '_closer_pw', true);
-    $url = get_permalink($post_id) . '?closed=close&pw=' . $closer_pw;
+    $url = get_permalink($post_id) . '?closed=closed&pw=' . $closer_pw;
 
     $subject = 'To Close Your VCU Research Interest Network Request(Keep This)';
     $body = 'To close your research project, click on the link. ' . $url;
